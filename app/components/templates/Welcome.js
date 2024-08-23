d72faaaed7bb3a543d36de20941132116bdc2cd9
Yasas Banuka <yasasbanukaofficial@gmail.com>
2024-08-23 22:37:37 +0530

    added video background

--- app/components/templates/Welcome.js
+++ app/components/templates/Welcome.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import styles from './Welcome.css'; // Adjust the path according to your file structure
 
 const Welcome = () => {
     return (
-        <div>
+        <div className='videoContainer'>
+            <video muted autoPlay className='videoplayer'>
+                <source src="/effect1.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+            <div className='main-content'>
 
+            </div>
         </div>
-    )
+    );
 }
 
-export default Welcome
+export default Welcome;
