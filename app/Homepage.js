f12a311ca550c25b1828c3f3d8d93c941c9faefe
Yasas Banuka <yasasbanukaofficial@gmail.com>
2024-08-24 13:35:55 +0530

    removed fadein.js

--- app/Homepage.js
+++ app/Homepage.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Welcome from './components/templates/Welcome'
 import About from './components/templates/About'
-import Fadein from './components/molecules/Fadein'
 
 const Homepage = () => {
     return (
