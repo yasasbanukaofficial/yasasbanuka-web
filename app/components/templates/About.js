f5b72a461d6d911fbd3f2f446aae272322353e6e
Yasas Banuka <yasasbanukaofficial@gmail.com>
2024-08-25 21:48:20 +0530

    fixed errors

--- app/components/templates/About.js
+++ app/components/templates/About.js
@@ -1,7 +1,7 @@
 'use client'
 import { useEffect, useRef, useState } from 'react';
 import '@/app/components/templates/About.css';
-import mainSection from '../molecules/MainSection';
+import MainSection from '../molecules/MainSection';
 const About = () => {
     const [isVisible, setIsVisible] = useState(false);
     const imageRef = useRef(null);
